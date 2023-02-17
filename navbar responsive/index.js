2fedcd2022d77ca568e489b41a77256bea895d41
Umair Khan <125242896+mrumairkhan74@users.noreply.github.com>
2023-02-17 23:32:16 +0500

    Add files via upload

---
+++ navbar responsive/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+
+const fileContent = fs.readFileSync('index.htm');
+const server = http.createServer((req,res)=>{
+    res.writeHead(200, {'Content-type': 'text/html'});
+    res.end(fileContent);
+})
+server.listen(80, '127.0.0.1' , ()=>{
+    console.log("server in running 80");
+})
